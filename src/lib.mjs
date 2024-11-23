7fcc893da88c44d868525e5895d8b834d90f41db
Anton Lazarev <antonok35@gmail.com>
2024-11-22 18:49:09 -0800

    tweak prompt for better confidence in classification

--- src/lib.mjs
+++ src/lib.mjs
@@ -60,12 +60,22 @@ const keywordClassifierFallback = (innerText) => {
 const inPageAPI = {
   classifyInnerText: (innerText) => {
     let innerTextSnippet = innerText.slice(0, MAX_LENGTH)
+    let ifTruncated = ''
     if (innerTextSnippet.length !== innerText.length) {
       innerTextSnippet += '...'
+      ifTruncated = `the first ${MAX_LENGTH} characters of `
     }
-    const prompt = `An overlay element is considered to be a "cookie consent notice" if it notifies the user of the site's use of cookies or other storage technology, provides a link to a privacy policy or terms of service document, and/or offers the user choices for the usage of cookies on the site.
+    const prompt = `An overlay element is considered to be a "cookie consent notice" if it meets all of these criteria:
+1. it explicitly notifies the user of the site's use of cookies or other storage technology, such as: "We use cookies...", "This site uses...", etc.
+2. it offers the user choices for the usage of cookies on the site, such as: "Accept", "Reject", "Learn More", etc., or informs the user that their use of the site means they accept the usage of cookies.
 
-The following text was captured from the innerText of an HTML overlay element:
+**Note:** This definition does not include adult content notices or any other type of notice that is primarily focused on age verification or content restrictions. Cookie consent notices are specifically intended to inform users about the website's use of cookies and obtain their consent for such use.
+
+**Note:** A cookie consent notice should specifically relate to the site's use of cookies or other storage technology that stores data on the user's device, such as HTTP cookies, local storage, or session storage. Requests for permission to access geolocation information, camera, microphone, etc., do not fall under this category.
+
+**Note:** Do NOT classify a website header or footer as a "cookie consent notice". Website headers or footers may contain a list of links, possibly including a privacy policy, cookie policy, or terms of service document, but their primary purpose is navigational rather than informational.
+
+The following text was captured from ${ifTruncated}the innerText of an HTML overlay element:
 
 \`\`\`
 ${innerTextSnippet}
