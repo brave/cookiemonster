9e7eb227bf29a9c9918254cd6bff1e4124346aaa
Anton Lazarev <antonok35@gmail.com>
2025-01-23 18:02:42 -0800

    split inpage routine into more modular chunks

--- src/inpage/index.mjs
+++ src/inpage/index.mjs
@@ -1,171 +1,217 @@
 /* eslint-env browser */
 
-const asyncFilter = async (arr, predicate) => Promise.all(arr.map(predicate))
-  .then((results) => arr.filter((_v, index) => results[index]))
+const windowRect = {
+  left: 0,
+  right: window.innerWidth,
+  top: 0,
+  bottom: window.innerHeight
+}
 
-// This is the main routine that runs within a page and returns information about detected elements.
-// Function dependencies from the host can be exposed and accessed via the API exposed by `randomToken`.
-export async function inPageRoutine (randomToken, hostOverride) {
-  const hostAPI = ['getETLDP1', 'classifyCookieNoticeText', 'extractFrameText'].reduce((acc, v) => {
-    acc[v] = (...args) => window[randomToken](v, ...args)
-    return acc
-  }, {})
-
-  const fixedPositionElements = []
+/**
+ * Collects all elements from the DOM that match the provided criteria
+ * @param {Function} criteria
+ */
+const collectMatchingElements = (criteria) => {
+  const elements = []
   const walker = document.createTreeWalker(
     document.documentElement,
     NodeFilter.SHOW_ELEMENT,
-    el => {
-      if (el.tagName === 'BODY') {
-        return NodeFilter.FILTER_SKIP
-      }
-      const computedStyle = getComputedStyle(el).position
-      return (computedStyle === 'fixed' || computedStyle === 'sticky')
-        ? NodeFilter.FILTER_ACCEPT
-        : NodeFilter.FILTER_SKIP
-    }
+    e => criteria(e) ? NodeFilter.FILTER_ACCEPT : NodeFilter.FILTER_SKIP
   )
   while (walker.nextNode()) {
-    if (walker.currentNode.checkVisibility && walker.currentNode.checkVisibility()) {
-      fixedPositionElements.push(walker.currentNode)
-    }
+    elements.push(walker.currentNode)
   }
+  return elements
+}
 
-  const windowRect = {
-    left: 0,
-    right: window.innerWidth,
-    top: 0,
-    bottom: window.innerHeight
+/**
+ * Like Array.prototype.filter, but with support for async callbacks
+ */
+const asyncFilter = async (arr, predicate) => Promise.all(arr.map(predicate))
+  .then((results) => arr.filter((_v, index) => results[index]))
+
+/**
+ * Returns false for hidden elements, or elements outside of the bounds of the viewport
+ */
+const isVisible = node => {
+  if (!(node.checkVisibility && node.checkVisibility())) {
+    return false
   }
+  const nodeRect = node.getBoundingClientRect()
+  if (nodeRect.left >= windowRect.right ||
+      nodeRect.right <= windowRect.left ||
+      nodeRect.top >= windowRect.bottom ||
+      nodeRect.bottom <= windowRect.top) {
+    return false
+  }
+  return true
+}
 
-  const visibleElements = fixedPositionElements.filter(node => {
-    const nodeRect = node.getBoundingClientRect()
-    if (nodeRect.left >= windowRect.right ||
-        nodeRect.right <= windowRect.left ||
-        nodeRect.top >= windowRect.bottom ||
-        nodeRect.bottom <= windowRect.top) {
-      return false
-    }
-    return true
-  })
-
-  // filter out elements which contain a lot of predominantly first-party links
-  const thisHost = hostOverride !== undefined ? hostOverride : new URL(window.location.href).host
-  const thisDomain = await hostAPI.getETLDP1(thisHost)
-  const linkCheckedElements = await asyncFilter(visibleElements, async e => {
-    const linkPartiness = await Promise.all(Array.from(e.querySelectorAll('a[href]')).map(async a => {
-      const linkETLDP1 = await hostAPI.getETLDP1(new URL(a.href).host)
-      return thisDomain === linkETLDP1
-    }))
-    const linkPartinessCount = linkPartiness.reduce((acc, v) => {
-      if (v === true) {
-        acc.first += 1
-      } else {
-        acc.third += 1
-      }
-      return acc
-    }, { first: 0, third: 0 })
-
-    if (linkPartinessCount.first > 10 && linkPartinessCount.first > linkPartinessCount.third) {
-      return false
-    }
-
-    return true
-  })
-
-  const nonParentElements = []
-  if (linkCheckedElements.length > 0) {
-    for (let i = linkCheckedElements.length - 1; i >= 0; i--) {
+/**
+ * Returns only the elements that are *not* ancestors of any other input element
+ */
+const nonParentElements = elements => {
+  const results = []
+  if (elements.length > 0) {
+    for (let i = elements.length - 1; i >= 0; i--) {
       let container = false
-      for (let j = 0; j < linkCheckedElements.length; j++) {
-        if (i !== j && linkCheckedElements[i].contains(linkCheckedElements[j])) {
+      for (let j = 0; j < elements.length; j++) {
+        if (i !== j && elements[i].contains(elements[j])) {
           container = true
           break
         }
       }
       if (!container) {
-        nonParentElements.push(linkCheckedElements[i])
+        results.push(elements[i])
       }
     }
   }
+  return results
+}
+
+/**
+ * Returns false for elements that contain *both*:
+ *  - more than 10 first-party links, and
+ *  - more first-party links than third-party links
+ */
+const hasManyFirstPartyLinks = async (getETLDP1, thisDomain, e) => {
+  const linkPartiness = await Promise.all(Array.from(e.querySelectorAll('a[href]')).map(async a => {
+    const linkETLDP1 = await getETLDP1(new URL(a.href).host)
+    return thisDomain === linkETLDP1
+  }))
+  const linkPartinessCount = linkPartiness.reduce((acc, v) => {
+    if (v === true) {
+      acc.first += 1
+    } else {
+      acc.third += 1
+    }
+    return acc
+  }, { first: 0, third: 0 })
+
+  if (linkPartinessCount.first > 10 && linkPartinessCount.first > linkPartinessCount.third) {
+    return false
+  }
+
+  return true
+}
+
+/**
+ * Uses the hostAPI to classify the inner text of an HTML element as either a cookie notice or not.
+ * @param classifiersUsed {Set}
+ */
+const contentCheck = async (hostAPI, classifiersUsed, node) => {
+  const innerText = node.innerText
+  if (innerText.trim() === '') {
+    // some sites dump their cookie notices into iframes
+    const iframes = node.querySelectorAll('iframe')
+    for (const iframe of iframes) {
+      const innerText = await hostAPI.extractFrameText(iframe)
+      if (innerText.trim() === '') {
+        continue
+      }
+      const { classifier, classification } = await hostAPI.classifyCookieNoticeText(innerText)
+      classifiersUsed.add(classifier)
+      if (classification) {
+        return true
+      }
+    }
+    return false
+  }
+  const { classifier, classification } = await hostAPI.classifyCookieNoticeText(node.innerText)
+  classifiersUsed.add(classifier)
+  return classification
+}
+
+/**
+ * Given a "minimum viable cookie notice", i.e. some element that fully contains the text
+ * of a cookie notice and nothing further, collect:
+ * - the furthest ancestor element that **should** be hidden in addition to the notice (for e.g. overlays)
+ * - the furthest ancestor element that **can** be safely hidden without removing important page content
+ * - a number representing the tree depth between the previous two elements
+ * - any CSS `id`s within the tree between the previous two elements
+ */
+const expandDetectedCookieNotice = (node) => {
+  let outermostHideableElement = node
+  let innermostHideableElement = node
+  let hideableElementRange = 1
+  // Begin by looking upwards in the DOM for the outermost element that _can_ be hidden.
+  // When traversing upwards, this is the last element that adds no additional content to the
+  // original notice's `innerText`.
+  while (outermostHideableElement.parentElement &&
+    outermostHideableElement.parentElement.innerText.trim() === node.innerText.trim()) {
+    outermostHideableElement = outermostHideableElement.parentElement
+    hideableElementRange += 1
+    // In the meantime, look for the innermost element that _should_ be hidden.
+    // While traversing upwards, this is either the first element, or the last element that has:
+    // - full screen dimensions, AND
+    // - a blur filter and/or semi-transparent background style
+    const style = getComputedStyle(outermostHideableElement)
+    const nodeRect = outermostHideableElement.getBoundingClientRect()
+    if (nodeRect.left === windowRect.left &&
+      nodeRect.right === windowRect.right &&
+      nodeRect.top === windowRect.top &&
+      nodeRect.bottom === windowRect.bottom &&
+      (style.backgroundColor.startsWith('rgba(') ||
+      style.backdropFilter !== 'none')) {
+      innermostHideableElement = outermostHideableElement
+      hideableElementRange = 1
+    }
+  }
+
+  let n = innermostHideableElement
+  const hideableIds = []
+  do {
+    const id = n.getAttribute('id')
+    if (id !== null && id !== '') {
+      hideableIds.push(id)
+    }
+    if (n === outermostHideableElement) {
+      break
+    }
+    n = n.parentElement
+  } while (true)
+  return {
+    innermostHideableElement,
+    outermostHideableElement,
+    hideableElementRange,
+    hideableIds
+  }
+}
+
+/**
+ * This is the main routine that runs within a page and returns information about detected elements.
+ * Function dependencies from the host can be accessed via the API exposed by `randomToken`.
+ */
+export async function inPageRoutine (randomToken, hostOverride) {
+  const hostMethods = ['getETLDP1', 'classifyCookieNoticeText', 'extractFrameText']
+  const hostAPI = hostMethods.reduce((acc, v) => {
+    acc[v] = (...args) => window[randomToken](v, ...args)
+    return acc
+  }, {})
+
+  const thisHost = hostOverride !== undefined ? hostOverride : new URL(window.location.href).host
+  const thisDomain = await hostAPI.getETLDP1(thisHost)
 
   const classifiersUsed = new Set()
-  const contentCheckedElements = await asyncFilter(nonParentElements, async node => {
-    const innerText = node.innerText
-    if (innerText.trim() === '') {
-      // some sites dump their cookie notices into iframes
-      const iframes = node.querySelectorAll('iframe')
-      for (const iframe of iframes) {
-        const innerText = await hostAPI.extractFrameText(iframe)
-        if (innerText.trim() === '') {
-          continue
-        }
-        const { classifier, classification } = await hostAPI.classifyCookieNoticeText(innerText)
-        classifiersUsed.add(classifier)
-        if (classification) {
-          return true
-        }
-      }
+
+  // Collect cookie notices
+  let elements = collectMatchingElements(e => {
+    if (e.tagName === 'BODY') {
       return false
     }
-    const { classifier, classification } = await hostAPI.classifyCookieNoticeText(node.innerText)
-    classifiersUsed.add(classifier)
-    return classification
+    const computedStyle = getComputedStyle(e).position
+    return (computedStyle === 'fixed' || computedStyle === 'sticky') && isVisible(e)
   })
 
-  // Elements in `contentCheckedElements` constitute "minimum viable cookie notices".
-  // However, their parent elements may also be eligible for hiding:
-  // - parent elements with no other significant content **can** be safely hidden
-  // - parent elements with a full-page overlay **should** be hidden in addition to the notice
-  const identifiedCookieNotices = []
-  for (const node of contentCheckedElements) {
-    let outermostHideableElement = node
-    let innermostHideableElement = node
-    let hideableElementRange = 1
-    // Begin by looking upwards in the DOM for the outermost element that _can_ be hidden.
-    // When traversing upwards, this is the last element that adds no additional content to the
-    // original notice's `innerText`.
-    while (outermostHideableElement.parentElement &&
-      outermostHideableElement.parentElement.innerText.trim() === node.innerText.trim()) {
-      outermostHideableElement = outermostHideableElement.parentElement
-      hideableElementRange += 1
-      // In the meantime, look for the innermost element that _should_ be hidden.
-      // While traversing upwards, this is either the first element, or the last element that has:
-      // - full screen dimensions, AND
-      // - a blur filter and/or semi-transparent background style
-      const style = getComputedStyle(outermostHideableElement)
-      const nodeRect = outermostHideableElement.getBoundingClientRect()
-      if (nodeRect.left === windowRect.left &&
-        nodeRect.right === windowRect.right &&
-        nodeRect.top === windowRect.top &&
-        nodeRect.bottom === windowRect.bottom &&
-        (style.backgroundColor.startsWith('rgba(') ||
-        style.backdropFilter !== 'none')) {
-        innermostHideableElement = outermostHideableElement
-        hideableElementRange = 1
-      }
-    }
+  elements = await asyncFilter(elements, e =>
+    hasManyFirstPartyLinks(hostAPI.getETLDP1, thisDomain, e)
+  )
 
-    let n = innermostHideableElement
-    const hideableIds = []
-    do {
-      const id = n.getAttribute('id')
-      if (id !== null && id !== '') {
-        hideableIds.push(id)
-      }
-      if (n === outermostHideableElement) {
-        break
-      }
-      n = n.parentElement
-    } while (true)
-    const identifiedNotice = {
-      innermostHideableElement,
-      outermostHideableElement,
-      hideableElementRange,
-      hideableIds
-    }
-    identifiedCookieNotices.push(identifiedNotice)
-  }
+  elements = nonParentElements(elements)
+
+  elements = await asyncFilter(elements, (node) => contentCheck(hostAPI, classifiersUsed, node))
+
+  const identifiedCookieNotices = elements.map(expandDetectedCookieNotice)
 
   // Scroll blocking detection
   let scrollBlocked = false
